49c97279209bfa512d3a3d7a30222112e6d9482c
yifan <xieyifan@heshidai.com>
2018-07-30 12:23:44 +0800

    注释rabbitMQ

--- subs/producter.py
+++ subs/producter.py
@@ -1,23 +1,23 @@
-import pika
-
-
-# 创建连接
-class Producer:
-    def __init__(self):
-        self.user_pwd = pika.PlainCredentials('xie', 'Fs9006')
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters('192.168.102.180', credentials=self.user_pwd, virtual_host='/vhost/fan'))
-
-    def pub(self, body):
-        # 创建一个频道
-        channel = self.connection.channel()
-        channel.queue_declare(queue="hello")
-        channel.basic_publish(exchange='', routing_key='hello', body=body)
-
-    def close(self):
-        self.connection.close()
-
-
-if __name__ == '__main__':
-    p = Producer()
-    p.pub("我是一个人")
+# import pika
+#
+#
+# # 创建连接
+# class Producer:
+#     def __init__(self):
+#         self.user_pwd = pika.PlainCredentials('xie', 'Fs9006')
+#         self.connection = pika.BlockingConnection(
+#             pika.ConnectionParameters('192.168.102.180', credentials=self.user_pwd, virtual_host='/vhost/fan'))
+#
+#     def pub(self, body):
+#         # 创建一个频道
+#         channel = self.connection.channel()
+#         channel.queue_declare(queue="hello")
+#         channel.basic_publish(exchange='', routing_key='hello', body=body)
+#
+#     def close(self):
+#         self.connection.close()
+#
+#
+# if __name__ == '__main__':
+#     p = Producer()
+#     p.pub("我是一个人")
